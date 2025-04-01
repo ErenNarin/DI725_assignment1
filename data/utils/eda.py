723a807b5f2c149074ca2ca056d3e43020a54738
eren <erennarin.92@gmail.com>
2025-04-01 16:44:01 +0300

    eda plots are logged to wandb

--- data/utils/eda.py
+++ data/utils/eda.py
@@ -1,4 +1,5 @@
 import os
+import wandb
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -21,17 +22,19 @@ def load_data():
 
 
 def plot_sentiment_distribution(df, target):
-    plt.figure(figsize=(6, 6))
+    fig = plt.figure(figsize=(6, 6))
     sns.countplot(data=df, x=target, palette="coolwarm")
     plt.title(f"Distribution of {target}")
     plt.xlabel(target)
     plt.ylabel("Count")
     plt.show()
+    wandb.log({f"Distribution of {target}": wandb.Image(fig)})
 
 
 def _chi_square_test(df, feature, target):
     contingency_table = pd.crosstab(df[feature], df[target])
     chi2, p, _, _ = chi2_contingency(contingency_table)
+
     return chi2, p
 
 
@@ -47,7 +50,7 @@ def plot_feature_importance(df, target):
     for param, values in influential_params:
         print(f"{param}: Chi2 Score = {values['Chi2 Score']:.2f}, P-Value = {values['P-Value']:.5f}")
 
-    plt.figure(figsize=(10, 10))
+    fig = plt.figure(figsize=(10, 10))
     params = [param[0] for param in influential_params]
     chi2_scores = [param[1]['Chi2 Score'] for param in influential_params]
     sns.barplot(x=params, y=chi2_scores, palette="coolwarm")
@@ -56,11 +59,12 @@ def plot_feature_importance(df, target):
     plt.xlabel("Features")
     plt.ylabel("Chi-Square Score")
     plt.show()
+    wandb.log({"Feature Importance Based on Chi-Square Scores": wandb.Image(fig)})
 
 
 def plot_sentiment_distribution_by_feature(df, features, target):
     for col in features:
-        plt.figure(figsize=(8, 8))
+        fig = plt.figure(figsize=(8, 8))
         sns.countplot(data=df, x=col, hue=target, palette="coolwarm")
         plt.title(f"Sentiment Distribution by {col}")
         plt.xticks(rotation=45, ha='right')
@@ -68,3 +72,4 @@ def plot_sentiment_distribution_by_feature(df, features, target):
         plt.ylabel("Count")
         plt.legend(title='Sentiment')
         plt.show()
+        wandb.log({f"Sentiment Distribution by {col}": wandb.Image(fig)})
